b4bbc8baf24759e268e383335f971e366e4ae9d7
DESKTOP-B3BDLVD\blooo <jkim200@ncstudents.niagaracollege.ca>
2024-01-23 16:44:22 -0500

    Kim: Players

---
+++ WMBA-4/WMBA-4/Controllers/PlayerController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WMBA_4.Data;
+using WMBA_4.Models;
+
+namespace WMBA_4.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly WMBA_4_Context _context;
+
+        public PlayerController(WMBA_4_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Player
+        public async Task<IActionResult> Index()
+        {
+            var wMBA_4_Context = _context.Players.Include(p => p.Team);
+            return View(await wMBA_4_Context.ToListAsync());
+        }
+
+        // GET: Player/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Players == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players
+                .Include(p => p.Team)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // GET: Player/Create
+        public IActionResult Create()
+        {
+            ViewData["TeamID"] = new SelectList(_context.Teams, "ID", "Name");
+            return View();
+        }
+
+        // POST: Player/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,MemberID,FirstName,LastName,JerseyNumber,TeamID")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(player);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamID"] = new SelectList(_context.Teams, "ID", "Name", player.TeamID);
+            return View(player);
+        }
+
+        // GET: Player/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Players == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            ViewData["TeamID"] = new SelectList(_context.Teams, "ID", "Name", player.TeamID);
+            return View(player);
+        }
+
+        // POST: Player/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,MemberID,FirstName,LastName,JerseyNumber,TeamID")] Player player)
+        {
+            if (id != player.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(player);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlayerExists(player.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeamID"] = new SelectList(_context.Teams, "ID", "Name", player.TeamID);
+            return View(player);
+        }
+
+        // GET: Player/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Players == null)
+            {
+                return NotFound();
+            }
+
+            var player = await _context.Players
+                .Include(p => p.Team)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return View(player);
+        }
+
+        // POST: Player/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Players == null)
+            {
+                return Problem("Entity set 'WMBA_4_Context.Players'  is null.");
+            }
+            var player = await _context.Players.FindAsync(id);
+            if (player != null)
+            {
+                _context.Players.Remove(player);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlayerExists(int id)
+        {
+          return _context.Players.Any(e => e.ID == id);
+        }
+    }
+}
