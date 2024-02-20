0ad30776486bdeec166b9cae6a50f14b17eec9e7
Edward Rey Norris <enorris1@ncstudents.niagaracollege.ca>
2024-02-19 19:21:56 -0500

    created Staff and add it to side Bar

---
+++ WMBA-4/WMBA-4/Controllers/StaffController.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WMBA_4.CustomControllers;
+using WMBA_4.Data;
+using WMBA_4.Models;
+using WMBA_4.Utilities;
+
+namespace WMBA_4.Controllers
+{
+    public class StaffController : ElephantController
+    {
+        private readonly WMBA_4_Context _context;
+
+        public StaffController(WMBA_4_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Staff
+        public async Task<IActionResult> Index(string SearchString, int? RoleID, int? CoachID, int? page, int? pageSizeID,
+            string actionButton, string sortDirection = "asc", string sortField = "Staff")
+        {
+
+            var staff = from r in _context.Staff
+                                    .Include(r => r.Roles)
+                                    .Where(s => s.Status == true)
+                                    .AsNoTracking()
+                          select r;
+
+            //sorting sortoption array
+            string[] sortOptions = new[] { "Staff", "Role", "Status" };
+
+            //filter
+            if (RoleID.HasValue)
+            {
+                staff = staff.Where(r => r.RoleId == RoleID);
+            }
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                staff = staff.Where(p => p.LastName.ToUpper().Contains(SearchString.ToUpper())
+                                       || p.FirstName.ToUpper().Contains(SearchString.ToUpper()));
+            }
+
+            if (!String.IsNullOrEmpty(actionButton)) //Form Submitted!
+            {
+                page = 1;//Reset page to start
+                         //sorting
+                if (!String.IsNullOrEmpty(actionButton)) //Form Submitted!
+                {
+                    if (sortOptions.Contains(actionButton))//Change of sort is requested
+                    {
+                        if (actionButton == sortField) //Reverse order on same field
+                        {
+                            sortDirection = sortDirection == "asc" ? "desc" : "asc";
+                        }
+                        sortField = actionButton;//Sort by the button clicked
+                    }
+                }
+                if (sortField == "Staff")
+                {
+                    if (sortDirection == "asc")
+                    {
+                        staff = staff
+                            .OrderBy(p => p.FirstName)
+                            .ThenBy(p => p.LastName);
+                    }
+                    else
+                    {
+                        staff = staff
+                            .OrderByDescending(p => p.FirstName)
+                            .ThenByDescending(p => p.LastName);
+                    }
+                }
+                else if (sortField == "Role")
+                {
+                    if (sortDirection == "asc")
+                    {
+                        staff = staff
+                            .OrderBy(r => r.Roles);
+                    }
+                    else
+                    {
+                        staff = staff
+                            .OrderByDescending(r => r.Roles);
+                    }
+                }
+            }
+
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+            ViewData["RoleID"] = new SelectList(_context.Roles, "ID", "Name");
+
+            // Handle Paging
+            int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID);
+            ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
+            var pagedData = await PaginatedList<Staff>.CreateAsync(staff.AsNoTracking(), page ?? 1, pageSize);
+
+
+            return View(pagedData); ;
+        }
+
+        // GET: Staff/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Staff == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff
+                .Include(s => s.Roles)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // GET: Staff/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Roles, "ID", "ID");
+            return View();
+        }
+
+        // POST: Staff/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FirstName,LastName,Email,Status,RoleId")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(staff);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "ID", "ID", staff.RoleId);
+            return View(staff);
+        }
+
+        // GET: Staff/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Staff == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff.FindAsync(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "ID", "ID", staff.RoleId);
+            return View(staff);
+        }
+
+        // POST: Staff/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,FirstName,LastName,Email,Status,RoleId")] Staff staff)
+        {
+            if (id != staff.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(staff);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StaffExists(staff.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "ID", "ID", staff.RoleId);
+            return View(staff);
+        }
+
+        // GET: Staff/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Staff == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff
+                .Include(s => s.Roles)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Staff == null)
+            {
+                return Problem("Entity set 'WMBA_4_Context.Staff'  is null.");
+            }
+            var staff = await _context.Staff.FindAsync(id);
+            if (staff != null)
+            {
+                _context.Staff.Remove(staff);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StaffExists(int id)
+        {
+          return _context.Staff.Any(e => e.ID == id);
+        }
+    }
+}
