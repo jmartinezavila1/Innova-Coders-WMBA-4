c85a760f17785d7d9b3a87c3fe9ec1a9315fe96c
mgonzalesgonzal1 <mgonzalesgonzal1@ncstudents.niagaracollege.ca>
2024-02-21 14:19:53 -0500

    Fix Game Assignments - Edit Game, add edit game by division

---
+++ WMBA-4/WMBA-4/ViewModels/GameEditVM.cs
@@ -0,0 +1,20 @@
+﻿using WMBA_4.Models;
+
+namespace WMBA_4.ViewModels
+{
+    public class GameEditVM
+    {
+        public Game Game { get; set; }
+        public int DivisionID { get; set; }
+        public int Team1ID { get; set; }
+        public int Team2ID { get; set; }
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+
+        public DateTime Date { get; set; }
+        public int LocationID { get; set; }
+        public int SeasonID { get; set; }
+        public int GameTypeID { get; set; }
+
+    }
+}
