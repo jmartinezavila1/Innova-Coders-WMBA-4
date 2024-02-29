75d5e9d2fc9b9205687bb19bdb8cdb838b5352a0
CrisMC01 <cmurguiacozar1@ncstudents.niagaracollege.ca>
2024-02-28 23:49:57 -0500

    Changes to Inplay fro saving info

---
+++ WMBA-4/WMBA-4/ViewModels/InPlayMV.cs
@@ -0,0 +1,16 @@
+﻿namespace WMBA_4.ViewModels
+{
+    public class InPlayMV
+    {
+        public int InplayID { get; set; }
+        public int? PlayerBattingId { get; set; }
+        public string PlayerBattingName { get; set; }
+        public int? PlayerInBase1Id { get; set; }
+        public string PlayerInBase1Name { get; set; }
+        public int? PlayerInBase2Id { get; set; }
+        public string PlayerInBase2Name { get; set; }
+        public int? PlayerInBase3Id { get; set; }
+        public string PlayerInBase3Name { get; set; }
+    }
+}
+    
