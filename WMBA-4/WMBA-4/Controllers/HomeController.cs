50c553adefdb37ef2b7b247e688539664a72ce91
jiyoungkim813 <jkim200@ncstudents.niagaracollege.ca>
2024-02-29 20:13:00 -0500

    Home Index: -Deveoped upcoming game. -Showing today's or upcoming games only

--- WMBA-4/WMBA-4/Controllers/HomeController.cs
+++ WMBA-4/WMBA-4/Controllers/HomeController.cs
@@ -16,32 +16,37 @@ namespace WMBA_4.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            IQueryable<Game> games = _context.Games
-           .Include(g => g.GameType)
-           .Include(g => g.Location)
-           .Include(g => g.Season)
-           .Include(t => t.TeamGames)
-               .ThenInclude(t => t.Team)
-                   .ThenInclude(d => d.Division);
+            var today = DateTime.Today;
+
+            var games = await _context.Games
+                .Include(g => g.GameType)
+                .Include(g => g.Location)
+                .Include(g => g.Season)
+                .Include(t => t.TeamGames)
+                    .ThenInclude(t => t.Team)
+                        .ThenInclude(d => d.Division)
+                .Where(g => g.Date >= today) // Filter games starting from today
+                .OrderBy(g => g.Date) // Order games by date
+                .ToListAsync();
 
             var divisionCount = await _context.Divisions
-            .Where(d => d.Status == true)
-            .CountAsync();
+                .Where(d => d.Status == true)
+                .CountAsync();
             var playerCount = await _context.Players
-            .Where(p => p.Status == true)
-            .CountAsync();
+                .Where(p => p.Status == true)
+                .CountAsync();
             var gameCount = await _context.Games
-            .Where(g => g.Status == true)
-            .CountAsync();
+                .Where(g => g.Status == true)
+                .CountAsync();
             var teamCount = await _context.Teams
-           .Where(t => t.Status == true)
-           .CountAsync();
+                .Where(t => t.Status == true)
+                .CountAsync();
 
             ViewBag.DivisionCount = divisionCount;
             ViewBag.PlayerCount = playerCount;
             ViewBag.GameCount = gameCount;
             ViewBag.TeamCount = teamCount;
-            ViewBag.TeamName= _context.Teams.FirstOrDefault()?.Name;
+            ViewBag.TeamName = _context.Teams.FirstOrDefault()?.Name;
 
             return View(games);
         }
