b2c87d04f300afaae64512ae62d9be4e4bc954a9
jmartinezavila1 <jmartinezavila1@ncstudents.niagaracollege.ca>
2024-03-20 14:25:23 -0400

    Adding Use Roles Managing View

---
+++ WMBA-4/WMBA-4/ViewModels/UsersVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace WMBA_4.ViewModels
+{
+    public class UserVM
+    {
+        public string ID { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Display(Name = "Roles")]
+        public List<string> UserRoles { get; set; }
+    }
+    
+
+}
