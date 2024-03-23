79bfb8756b9873b413f907e53c5056780fe2c1ce
jmartinezavila1 <jmartinezavila1@ncstudents.niagaracollege.ca>
2024-03-23 12:18:36 -0400

    Adding nav menu in Team, adding authorization to Division. Everyone can access to Division (te requierments didnt say anything about that)

--- WMBA-4/WMBA-4/Controllers/DivisionController.cs
+++ WMBA-4/WMBA-4/Controllers/DivisionController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ using WMBA_4.Utilities;
 
 namespace WMBA_4.Controllers
 {
+    [Authorize]
     public class DivisionController : ElephantController
     {
         private readonly WMBA_4_Context _context;
