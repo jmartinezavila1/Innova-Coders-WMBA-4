a901594b3953d97e02b199425df31ce3fb0eb0ba
CrisMC01 <cmurguiacozar1@ncstudents.niagaracollege.ca>
2024-03-23 18:56:44 -0400

    --Developed End Season Feature. When the Admin decide to End season the system download a report of everything and truncate de tables I am just a judjusting the import file since is not working properly after end season.

---
+++ WMBA-4/WMBA-4/ViewModels/GameEndSeasonVM.cs
@@ -0,0 +1,23 @@
+﻿namespace WMBA_4.ViewModels
+{
+    public class GameEndSeasonVM
+    {
+        public string ID { get; set; }
+
+        public string Date { get; set; }
+
+        public string LocationName { get; set; }
+
+        public string DivisionName { get; set; }
+
+        public string TeamHome { get; set; }
+        public int scoreHome { get; set; }
+
+        public string TeamVisitor { get; set; }
+
+        public int scoreVisitor { get; set; }
+
+        public bool Status { get; set; }
+
+    }
+}
---
+++ WMBA-4/WMBA-4/ViewModels/PlayerEndSeasonVM.cs
@@ -0,0 +1,15 @@
+﻿namespace WMBA_4.ViewModels
+{
+    public class PlayerEndSeasonVM
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MemberID { get; set; }
+        public int JerseyNumber { get; set; }
+        public string Season { get; set; }
+        public string DivisionName { get; set; }
+        public string ClubName { get; set; }
+        public string Team { get; set; }
+    }
+}
