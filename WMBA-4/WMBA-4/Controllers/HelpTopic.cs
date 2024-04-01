d7f664cfd10a05345556fd222577011ed27a3653
Edward Rey Norris <enorris1@ncstudents.niagaracollege.ca>
2024-03-31 23:58:04 -0400

    help topic page

---
+++ WMBA-4/WMBA-4/Controllers/HelpTopic.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WMBA_4.Controllers
+{
+    public class HelpTopic : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
