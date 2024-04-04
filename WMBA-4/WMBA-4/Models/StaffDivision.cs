8de524f6bc85b7874625dcfe8564421eb57ea1e6
CrisMC01 <cmurguiacozar1@ncstudents.niagaracollege.ca>
2024-04-03 23:43:04 -0400

    --I added a new Model to the DB named StaffDivision to hold the relationship between Convenors and Divisions. So A added it to Context, Created the Model, and aded seed data. -Additionaly, I separated the Team stats and the player stats. Need to work more in stats

---
+++ WMBA-4/WMBA-4/Models/StaffDivision.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WMBA_4.Models
+{
+    public class StaffDivision
+    {
+        [Display(Name = "Division")]
+        public int DivisionID { get; set; }
+        public Division Division { get; set; }
+
+        [Display(Name = "Staff")]
+        public int StaffID { get; set; }
+        public Staff Staff { get; set; }
+    }
+}
